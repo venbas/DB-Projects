e08e245b7da68e3662656ebc57f7cdb57ad27067
Venkat B <venkat.baskaran@tcs.com>
2016-05-14 21:47:48 +0530

    commit ayitaen

--- Adv2012Test/Person/Tables/Password.sql
+++ Adv2012Test/Person/Tables/Password.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [Person].[Password] (
     [BusinessEntityID] INT              NOT NULL,
-    [PasswordHash]     VARCHAR (128)    NOT NULL,
+    [PasswordHash]     VARCHAR (128)    NOT NULL, --test
     [PasswordSalt]     VARCHAR (10)     NOT NULL,
     [rowguid]          UNIQUEIDENTIFIER CONSTRAINT [DF_Password_rowguid] DEFAULT (newid()) ROWGUIDCOL NOT NULL,
     [ModifiedDate]     DATETIME         CONSTRAINT [DF_Password_ModifiedDate] DEFAULT (getdate()) NOT NULL,
